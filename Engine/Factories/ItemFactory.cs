9dddd689ac5c071c5e17e636ca938ff5dc5f21f6
MattMauzy <matthew.mauzy@snhu.edu>
2019-01-12 18:44:27 -0600

    Added items to XML file

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
@@ -10,37 +12,25 @@ namespace Engine.Factories
 {
     public static class ItemFactory
     {
+        private const string GAME_DATA_FILENAME = ".\\GameData\\GameItems.xml";
+
         private static readonly List<GameItem> _standardGameItems = new List<GameItem>();
 
         static ItemFactory()
         {
-            // Weapons
-            BuildWeapon(1001, "Sharp Stick", 1, 1, 2);
-            BuildWeapon(1002, "Police Baton", 1, 1, 2);
-            BuildWeapon(1005, "Legendary Sword", 100, 150, 200);
+            if(File.Exists(GAME_DATA_FILENAME))
+            {
+                XmlDocument data = new XmlDocument();
+                data.LoadXml(File.ReadAllText(GAME_DATA_FILENAME));
 
-            BuildWeapon(1010, "Laser", 0, 0, 10);
-            BuildWeapon(1011, "Club", 0, 0, 5);
-            BuildWeapon(1012, "Goo", 0, 0, 3);
-
-            // Health 
-            BuildHealingItem(8001, "Bread", 1, 5);
-            BuildMiscellaneousItem(8002, "Health Potion", 25);
-
-            // Recipe Items
-            BuildMiscellaneousItem(3000, "Sword Hilt", 2);
-            BuildMiscellaneousItem(3001, "Iron Ingot", 2);
-            BuildMiscellaneousItem(3002, "Steel Ingot", 3);
-
-            //Random Junk
-            BuildMiscellaneousItem(5003, "Slime", 1);
-            BuildMiscellaneousItem(5004, "Jar of Slobber", 2);
-            BuildMiscellaneousItem(5005, "Shaving Cream", 1);
-            BuildMiscellaneousItem(5006, "Old Pizza", 4);
-            BuildMiscellaneousItem(5007, "Robot Parts", 40);
-
-            //Game Items
-            BuildMiscellaneousItem(9001, "Prison Key", 3);
+                LoadItemsFromNodes(data.SelectNodes("/GameItems/Weapons/Weapon"));
+                LoadItemsFromNodes(data.SelectNodes("/GameItems/HealingItems/HealingItem"));
+                LoadItemsFromNodes(data.SelectNodes("/GameItems/MiscellaneousItems/MiscellaneousItem"));
+            }
+            else
+            {
+                throw new FileNotFoundException($"Missing data file: {GAME_DATA_FILENAME}");
+            }
         }
 
         public static GameItem CreateGameItem(int itemTypeID)
@@ -48,31 +38,80 @@ namespace Engine.Factories
             return _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID)?.Clone();
         }
 
-        private static void BuildMiscellaneousItem(int id, string name, int price)
-        {
-            _standardGameItems.Add(new GameItem(GameItem.ItemCategory.Miscellaneous, id, name, price));
-        }
-
-        private static void BuildWeapon(int id, string name, int price,
-            int minDamage, int maxDamage)
-        {
-            GameItem weapon = new GameItem(GameItem.ItemCategory.Weapon, id, name, price, true);
-
-            weapon.Action = new AttackWithWeapon(weapon, minDamage, maxDamage);
-
-            _standardGameItems.Add(weapon);
-        }
-
-        private static void BuildHealingItem(int id, string name, int price, int hitPointsHealed)
-        {
-            GameItem item = new GameItem(GameItem.ItemCategory.Consumable, id, name, price);
-            item.Action = new Heal(item, hitPointsHealed);
-            _standardGameItems.Add(item);
-        }
-
         public static string ItemName(int itemTypeID)
         {
             return _standardGameItems.FirstOrDefault(i => i.ItemTypeID == itemTypeID)?.Name ?? "";
+        }
+
+        private static void LoadItemsFromNodes(XmlNodeList nodes)
+        {
+            if(nodes == null)
+            {
+                return;
+            }
+
+            foreach(XmlNode node in nodes)
+            {
+                GameItem.ItemCategory itemCategory = DetermineItemCategory(node.Name);
+
+                GameItem gameItem =
+                    new GameItem(itemCategory,
+                    GetXmlAttributeAsInt(node, "ID"),
+                    GetXmlAttributeAsString(node, "Name"),
+                    GetXmlAttributeAsInt(node, "Price"),
+                    itemCategory == GameItem.ItemCategory.Weapon);
+
+                if(itemCategory == GameItem.ItemCategory.Weapon)
+                {
+                    gameItem.Action =
+                        new AttackWithWeapon(gameItem,
+                        GetXmlAttributeAsInt(node, "MinDamage"),
+                        GetXmlAttributeAsInt(node, "MaxDamage"));
+                }
+                else if(itemCategory == GameItem.ItemCategory.Consumable)
+                {
+                    gameItem.Action =
+                        new Heal(gameItem,
+                        GetXmlAttributeAsInt(node, "HitPointsHealed"));
+                }
+
+                _standardGameItems.Add(gameItem);
+            }
+        }
+
+        private static GameItem.ItemCategory DetermineItemCategory(string itemType)
+        {
+            switch(itemType)
+            {
+                case "Weapon":
+                    return GameItem.ItemCategory.Weapon;
+                case "HealingItem":
+                    return GameItem.ItemCategory.Consumable;
+                default:
+                    return GameItem.ItemCategory.Miscellaneous;
+            }
+        }
+
+        private static int GetXmlAttributeAsInt(XmlNode node, string attributeName)
+        {
+            return Convert.ToInt32(GetXmlAttribute(node, attributeName));
+        }
+
+        private static string GetXmlAttributeAsString(XmlNode node, string attributeName)
+        {
+            return GetXmlAttribute(node, attributeName);
+        }
+
+        private static string GetXmlAttribute(XmlNode node, string attributeName)
+        {
+            XmlAttribute attribute = node.Attributes?[attributeName];
+
+            if(attribute == null)
+            {
+                throw new ArgumentException($"The attribute ' {attributeName}' dows not exist");
+            }
+
+            return attribute.Value;
         }
     }
 }
